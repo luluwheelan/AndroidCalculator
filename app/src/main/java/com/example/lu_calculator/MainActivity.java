268a62473535cdc9937e8968f1b5f2785ba750cb
Lulu <luluwheelan@gmail.com>
2019-04-07 12:38:49 -0400

    Declare and Assign variables.

--- app/src/main/java/com/example/lu_calculator/MainActivity.java
+++ app/src/main/java/com/example/lu_calculator/MainActivity.java
@@ -8,13 +8,179 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+//    Declare variables
+    TextView expression, result, one, two, three,
+        four, five, six, seven, eight, nine, zero,
+    mul, minus, dot, equal, plus, divide;
+    String input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Assign variables
+        expression = findViewById(R.id.expression);
+        result = findViewById(R.id.result);
+        expression.setText("");
+        result.setText("");
+
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        five = findViewById(R.id.five);
+        six = findViewById(R.id.six);
+        seven = findViewById(R.id.seven);
+        eight = findViewById(R.id.eight);
+        nine = findViewById(R.id.nine);
+        zero = findViewById(R.id.zero);
+
+        mul = findViewById(R.id.mul);
+        minus = findViewById(R.id.minus);
+        plus = findViewById(R.id.plus);
+        dot = findViewById(R.id.dot);
+        equal = findViewById(R.id.equal);
+        divide = findViewById(R.id.divide);
+
+//Number click event
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "1");
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "2");
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "3");
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "4");
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "5");
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "6");
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "7");
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "8");
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "9");
+            }
+        });
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "0");
+            }
+        });
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + ".");
+            }
+        });
+
+//        Operator click event
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "+");
+            }
+        });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "-");
+            }
+        });
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "*");
+            }
+        });
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clearScreen();
+                input = expression.getText().toString();
+                expression.setText(input + "/");
+            }
+        });
+
+
+//        public void clearScreen(){
+//            expression = expression.getText().toString();
+//            if(processor.contains(developedBy)){
+//                tvProcessor.setText("");
+//                tvResult.setText("");
+//            }
+//            developedCounter = 0;
+//        }
 
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
